d2c2476e1eb7ed6b89822c733922913426e90a91
maurojh <maurojh@gmail.com>
2019-08-06 19:42:28 -0300

    arquivos prontos

---
+++ verde.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Vermelho</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+</head>
+
+<body <?php 
+      $cor="green" ; 
+      echo "style='background-color:$Cor'"; 
+      ?>
+    >
+    <div class="container-fluid">
+        <div class="columns">
+            <div class="column">
+                <h1>Resultado final.</h1>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ vermelho.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Vermelho</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+</head>
+
+<body <?php 
+      $cor="red"; 
+      echo "style='background-color:$cor'"; ?>
+    >
+    <div class="container-fluid">
+        <div class="columns">
+            <div class="column">
+                <h1>Resultado final.</h1>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
